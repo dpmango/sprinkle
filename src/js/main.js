fdc4b43267fbf0c19caef06d56d2967c686ded46
Sergey Khmelevskoy <xs290@me.com>
2017-11-15 01:13:11 +0300

    scroll nav

--- src/js/main.js
+++ src/js/main.js
@@ -60,15 +60,6 @@ $(document).ready(function(){
 		e.preventDefault();
 	});
 
-	// Smoth scroll
-	$('a[href^="#section"]').click( function() {
-    var el = $(this).attr('href');
-    $('body, html').animate({
-        scrollTop: $(el).offset().top}, 1000);
-    return false;
-	});
-
-
   // TELEPORT PLUGIN
   $('[js-teleport]').each(function (i, val) {
     var self = $(val)
@@ -77,9 +68,6 @@ $(document).ready(function(){
     var conditionMedia = $(val).data('teleport-condition').substring(1);
     var conditionPosition = $(val).data('teleport-condition').substring(0, 1);
 
-    console.log(target);
-
-    
     if (target && objHtml && conditionPosition) {
       _window.on('resize', throttle(function () {
         teleport()
@@ -105,5 +93,79 @@ $(document).ready(function(){
       teleport();
     }
   });
+
+  // SCROLL FUNC
+  function revealScroll(){
+    var title = $('.intro__title');
+    var subtitle = $('.intro__description');
+    var btnAppStore = $('.intro__store');
+    var btnMore = $('.intro__btn');
+    var benefits = $('.benefits');
+
+    if ( _window.width() > 768 ){
+      var wScroll = _window.scrollTop();
+      var container = _document
+      // var scrollPercent = 100 * _window.scrollTop() / ( container.height() - _window.height());
+      var scrollMagnifier = (container.height() - _window.height()) / 2
+
+
+      title.css({
+        'opacity': 1 - ((wScroll / scrollMagnifier / 1.2) + 0.2)
+      })
+      subtitle.css({
+        'opacity': 1 - (wScroll / scrollMagnifier / 1.2)
+      })
+
+      btnAppStore.css({
+        'transform': "translate3d(0,-" + wScroll / scrollMagnifier * 64 + "px,0)"
+      })
+
+      btnMore.css({
+        opacity: 1 - wScroll / scrollMagnifier,
+        'transform': "translate3d(0,-" + wScroll / scrollMagnifier * 64 + "px,0)"
+      })
+
+      benefits.css({
+        opacity: 0 + wScroll / (scrollMagnifier * 2 ),
+        'transform': "translate3d(0,-" + wScroll / scrollMagnifier * 145 + "px,0)"
+      })
+
+    } else {
+      title.css({
+        'opacity': 1
+      })
+      subtitle.css({
+        'opacity': 1
+      })
+
+      btnAppStore.css({
+        'transform': "translate3d(0,-" + 0 + "px,0)"
+      })
+
+      btnMore.css({
+        'opacity': 1,
+        'transform': "translate3d(0,-" + 0 + "px,0)"
+      })
+
+      benefits.css({
+        'opacity': 1,
+        'transform': "translate3d(0,-" + 0 + "px,0)"
+      })
+    }
+
+  };
+
+  _window.on('scroll', throttle(revealScroll, 5));
+  _window.on('resize', throttle(revealScroll, 300));
+
+  revealScroll();
+
+  $('.intro__btn').on('click', function(e){
+    $('body, html').animate({
+        scrollTop: _document.height() - _window.height() }, 1000);
+    return false;
+
+    e.preventDefault();
+  })
 
 });
