df61642b20edd95e144f87d54020fdc714fad2e9
Sergey Khmelevskoy <xs290@me.com>
2017-11-14 20:59:12 +0300

    mobile

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
 
   function isRetinaDisplay() {
     if (window.matchMedia) {
-        var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
-        return (mq && mq.matches || (window.devicePixelRatio > 1));
+      var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+      return (mq && mq.matches || (window.devicePixelRatio > 1));
     }
   }
 
@@ -45,7 +45,7 @@ $(document).ready(function(){
 
   // svg support for laggy browsers
   svg4everybody();
-  // 
+  //
   // // Viewport units buggyfill
   // window.viewportUnitsBuggyfill.init({
   //   force: true,
@@ -62,11 +62,48 @@ $(document).ready(function(){
 
 	// Smoth scroll
 	$('a[href^="#section"]').click( function() {
-        var el = $(this).attr('href');
-        $('body, html').animate({
-            scrollTop: $(el).offset().top}, 1000);
-        return false;
+    var el = $(this).attr('href');
+    $('body, html').animate({
+        scrollTop: $(el).offset().top}, 1000);
+    return false;
 	});
 
+
+  // TELEPORT PLUGIN
+  $('[js-teleport]').each(function (i, val) {
+    var self = $(val)
+    var objHtml = $(val).html();
+    var target = $('[data-teleport-target=' + $(val).data('teleport-to') + ']');
+    var conditionMedia = $(val).data('teleport-condition').substring(1);
+    var conditionPosition = $(val).data('teleport-condition').substring(0, 1);
+
+    console.log(target);
+
+    
+    if (target && objHtml && conditionPosition) {
+      _window.on('resize', throttle(function () {
+        teleport()
+      }, 100));
+
+      function teleport() {
+        var condition;
+
+        if (conditionPosition === "<") {
+          condition = _window.width() < conditionMedia;
+        } else if (conditionPosition === ">") {
+          condition = _window.width() > conditionMedia;
+        }
+
+        if (condition) {
+          target.html(objHtml)
+          self.html('')
+        } else {
+          self.html(objHtml)
+          target.html("")
+        }
+      }
+      teleport();
+    }
+  });
 
 });
